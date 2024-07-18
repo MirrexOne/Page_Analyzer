3b2f6349ab3b53e57d129634f329b063d6c326dc
Alex Belovitsky <mirrexone@gmail.com>
2024-07-18 10:50:35 +0300

    Add test to save URL

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,11 +1,16 @@
 package hexlet.code;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
+import hexlet.code.model.Url;
+import hexlet.code.repository.UrlRepository;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
+import okhttp3.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
 
@@ -40,4 +45,15 @@ public class AppTest {
         }));
     }
 
+    @Test
+    public void testSaveUrl() throws SQLException {
+        String initialUrl = "https://github.com";
+        JavalinTest.test(app, (server, client) -> {
+            var requestBody = "url=" + initialUrl;
+            var response = client.post("/urls", requestBody);
+            assertThat(response.code()).isEqualTo(200);
+        });
+        Url url = UrlRepository.findByName(initialUrl).isPresent() ? UrlRepository.findByName(initialUrl).get() : null;
+        assertThat(url.getName()).isEqualTo(initialUrl);
+    }
 }
