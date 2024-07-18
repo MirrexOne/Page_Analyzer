3a1315a404eff1c7dca237929396907859c3e576
Alex Belovitsky <mirrexone@gmail.com>
2024-07-18 09:48:09 +0300

    Add test directory and first test case

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.javalin.Javalin;
+import io.javalin.testtools.JavalinTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    Javalin app;
+
+    @BeforeEach
+    public final void setApp() throws Exception {
+        app = App.getApp();
+    }
+
+    @Test
+    public void testRootPage() {
+        JavalinTest.test(app, ((server, client) -> {
+            var response = client.get("/");
+            assertThat(response.code()).isEqualTo(200);
+        }));
+    }
+
+
+}
