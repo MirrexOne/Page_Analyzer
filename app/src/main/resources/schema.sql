fa29b58e93d48e4802f28a9cffb823d14bb3fe5f
Alex Belovitsky <mirrexone@gmail.com>
2024-07-18 10:50:08 +0300

    Change SQL schema

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS urls;
 CREATE TABLE urls
 (
     id         SERIAL PRIMARY KEY NOT NULL,
-    name       VARCHAR(255) NOT NULL UNIQUE,
+    name       VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL
 )
