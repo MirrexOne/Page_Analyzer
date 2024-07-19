41c5a0bfbfdf7f7d6f1558eaded093a451f85b2c
Alex Belovitsky <mirrexone@gmail.com>
2024-07-19 09:37:01 +0300

    Add JAVA-DOC to getters and setter in BasePage class

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -5,18 +5,34 @@ public class BasePage {
     private String flash;
     private String flashType;
 
+    /**
+     * Get flash message.
+     * @return flash message
+     */
     public String getFlash() {
         return flash;
     }
 
+    /**
+     * Set flash message.
+     * @param flash - message for flash
+     */
     public void setFlash(String flash) {
         this.flash = flash;
     }
 
+    /**
+     * Get flash type  status.
+     * @return flash type status
+     */
     public String getFlashType() {
         return flashType;
     }
 
+    /**
+     * Set flash type status.
+     * @param flashType - flash type status
+     */
     public void setFlashType(String flashType) {
         this.flashType = flashType;
     }
